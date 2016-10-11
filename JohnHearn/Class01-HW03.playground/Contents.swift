729d44dcfc28a6293a492f4d08dd1c5baf7460ef
John Hearn <jhearn@gmail.com>
2016-10-11 09:41:27 -0700

    HW3 initial commit

---
+++ JohnHearn/Class01-HW03.playground/Contents.swift
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * *
+ * John Hearn            *
+ * Code Fellows 401: iOS *
+ * October 2016          *
+ * * * * * * * * * * * * */
+
+import UIKit
+
+/*
+ * Declare Identity protocol with id String
+ */
+
+/*
+ * Declare a ToDo class that has a text member of type String, and conforms to Identity protocol
+ */
+
+
+/*
+ * Define ObjectStore protocol with these functions: add:, remove:, objectAtIndex:, count, allObjects.
+ */
+
+ 
+/*
+ * Create Store class that will conform to ObjectStore protocol and implement required methods
+ */
+
+ 
+/*
+ * Demonstrate adding / removing of ToDo items.
+ */
