48893f9613d448d2b96478e3e9ab6bd12b96a3d6
John Hearn <jhearn@gmail.com>
2016-10-13 14:35:37 -0700

    initial commit for hw4 branch

---
+++ JohnHearn/Class01-HW04.playground/Contents.swift
@@ -0,0 +1,27 @@
+/* * * * * * * * * * * * *
+ * John Hearn            *
+ * Code Fellows 401: iOS *
+ * October 2016          *
+ * * * * * * * * * * * * */
+
+/*
+ * °  Declare Enum that conforms to Error. Your enum should have at least 5
+ *    cases and should use Associated Values.
+ * °  Declare a Person class with 3 member properties. Declare a Student
+ *    subclass that inherits from Person.
+ *    .  Both Your Student and Person class should have a variety of meaningful
+ *       Type and Instance Methods. Be creative.
+ *    .  In either of these 2 classes, demonstrate a computed property and
+ *       implement a property observer.
+ * °  Create a Classroom class that is a true singleton and has an array of 
+ *    Students.
+ *    .  Add accessor methods to Classroom to add and remove from the array.
+ */
+
+
+import UIKit
+
+
+
+
+
