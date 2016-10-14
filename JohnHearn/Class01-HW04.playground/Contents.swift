15f20bb2b1de39f9b4a11f882d7278b251998e1f
John Hearn <jhearn@gmail.com>
2016-10-13 17:18:08 -0700

    Person and Student classes implemented with init()s, Date()s, and willSet/didSet

--- JohnHearn/Class01-HW04.playground/Contents.swift
+++ JohnHearn/Class01-HW04.playground/Contents.swift
@@ -18,8 +18,83 @@
  *    .  Add accessor methods to Classroom to add and remove from the array.
  */
 
-
 import UIKit
+
+extension Calendar {
+    static var gregorian : Calendar {
+        return Calendar(identifier: Calendar.Identifier.gregorian)
+    }
+}
+
+enum StudentError: Error {
+    case StudentNotFound
+    case DuplicateEntry (id: String)
+    case IneligibleToEnroll
+    case InvalidID
+    case InsufficientFunds (tuition: Int)
+}
+
+
+class Person {
+    var ssn: String
+    var name: (last: String, first: String)
+    var dateOfBirth: Date?
+
+    init(first: String, last: String, year: Int, month: Int, day: Int, ssn: String ){
+        self.name = (last, first)
+        self.dateOfBirth = DateComponents(calendar: Calendar.gregorian, year: year, month: month, day: day).date
+        self.ssn = ssn
+    }
+}
+
+class Student: Person {
+    var studentID: String{
+        return self.ssn + String(self.ssn.hash)
+    }
+    private var previousFunds: Int?
+    var funds: Int {
+        willSet(newFunds) {
+            self.previousFunds = self.funds
+            print("Funds being set: \(self.funds) -> \(newFunds).")
+
+        }
+        didSet {
+            print("Funds set to \(self.funds)")
+        }
+    }
+    var age: Int {
+        return Int( (Date().timeIntervalSince( self.dateOfBirth! )) / 31_557_600 )
+    }
+
+    init(first: String, last: String, year: Int, month: Int, day: Int, ssn: String, funds: Int) {
+        self.funds = funds
+        super.init(first: first, last: last, year: year, month: month, day: day, ssn: ssn)
+    }
+
+    func refundTuition() -> String {
+        let refundAmount = self.previousFunds! - self.funds
+        self.funds = self.previousFunds!
+
+        return "$\(refundAmount) should be paid to \(self.name.first) \(self.name.last)"
+    }
+}
+
+
+var person = Person(first: "Joe", last: "Mama", year: 1992, month: 10, day: 10, ssn: "234-56-7890")
+var student = Student(first: "Joe", last: "Mama", year: 1992, month: 10, day: 10, ssn: "234-56-7890", funds: 10000)
+
+student.age
+
+student.funds = 100001
+student.funds = 0
+print( student.refundTuition() )
+
+
+
+
+
+
+
 
 
 
